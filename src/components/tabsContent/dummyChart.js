2bbfb6519f815efb95174b211788591916bbe1a0
Alina Shtykh <alinashtykh@gmail.com>
2023-10-28 16:14:08 +0200

    added data and tabs content

---
+++ src/components/tabsContent/dummyChart.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DummyChart = () => {
+  return <div>dummyChart</div>;
+};
+
+export default DummyChart;
---
+++ src/components/tabsContent/dummyList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DummyList = () => {
+  return <div>dummyList</div>;
+};
+
+export default DummyList;
