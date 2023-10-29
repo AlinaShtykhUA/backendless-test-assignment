23c4c1151e6ef22ddccac498ae8059d3cc40f1d0
Alina Shtykh <alinashtykh@gmail.com>
2023-10-29 14:07:47 +0100

    added sort

--- src/components/TabsNav/TabsNav.js
+++ src/components/TabsNav/TabsNav.js
@@ -5,11 +5,13 @@ import "./tabsNav.css";
 const TabsNav = () => {
   return (
     <nav className="nav">
-      {tabs.map(({ id, title }) => (
-        <NavLink className="nav-link" key={id} to={id}>
-          {title}
-        </NavLink>
-      ))}
+      {tabs
+        .sort((a, b) => a.order - b.order)
+        .map(({ id, title }) => (
+          <NavLink className="nav-link" key={id} to={id}>
+            {title}
+          </NavLink>
+        ))}
     </nav>
   );
 };
